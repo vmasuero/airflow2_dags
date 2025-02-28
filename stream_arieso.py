73a49e1623d1aabfae0b16d87f22e44df2dc6a9e
Oci <valdo.masuero@gmail.com>
2025-02-27 22:31:20 -0300

    Agregando Sensor Kaflka y proceso

--- stream_arieso.py
+++ stream_arieso.py
@@ -6,6 +6,7 @@ from airflow.utils.decorators import apply_defaults
 from airflow.utils.dates import days_ago
 from airflow.decorators import dag, task
 from airflow.models import Variable
+from airflow.utils.task_group import TaskGroup
 
 from airflow.utils.dates import days_ago
 
@@ -38,8 +39,10 @@ KAFKA_EPS = [
     {'name':'clarochilelldr14-kafka', 'ip':'172.18.90.44'}
     
 ]
-
 KAFKA_TOPIC = 'Nsa5GAnalyticsStreamingFeedRecords'
+KAFKA_GROUPID = 'Airflow'
+KAFKA_NAMES = [x['name'] for x in KAFKA_EPS]
+
 
 POSTGRES_IP = 'postgres-viavi.ntv'
 POSTGRES_USER = 'admin'
@@ -70,10 +73,100 @@ REQUIRED_COLS = [
     'nrerab_overallaveragedownlinkthroughput'
 ]
 
+def process_message(msg_obj, broker_id:str) -> pd.DataFrame:
+    
+    def message_to_dict(msg):
+
+        result = {}
+        for field in msg.DESCRIPTOR.fields:
+            field_name = field.name
+
+            if field.label != field.LABEL_REPEATED:
+                if field.cpp_type == field.CPPTYPE_MESSAGE:
+                
+                    if not msg.HasField(field_name):
+                        continue
+                        
+                    value = getattr(msg, field_name)
+                    result[field_name] = message_to_dict(value)
+                    
+                else:
+                    result[field_name] = getattr(msg, field_name)
+                    
+            else:
+                value_list = getattr(msg, field_name)
+                
+                if value_list:
+                    result[field_name] = []
+                    
+                    for item in value_list:
+                        if field.cpp_type == field.CPPTYPE_MESSAGE:
+                            result[field_name].append(message_to_dict(item))
+                        else:
+                            result[field_name].append(item)
+        return result
+
+    def dict_to_dataframe(msg_dict:dict):
+        common_fields = {k: v for k, v in msg_dict.items() if k not in ['NrCells', 'NrErab']}
+    
+        rows = []
+        has_cells = 'NrCells' in msg_dict and msg_dict['NrCells']
+        has_erab = 'NrErab' in msg_dict and msg_dict['NrErab']
+        
+        if has_cells and has_erab:
+            for cell in msg_dict['NrCells']:
+                
+                for erab in msg_dict['NrErab']:
+                    row = common_fields.copy()
+                    for k, v in cell.items():
+                        row[f"NrCells_{k}"] = v
+                        
+                    for k, v in erab.items():
+                        row[f"NrErab_{k}"] = v
+                        
+                    rows.append(row)
+                    
+            return pd.DataFrame(rows)
+            
+        elif has_cells:
+            for cell in msg_dict['NrCells']:
+                row = common_fields.copy()
+                
+                for k, v in cell.items():
+                    row[f"NrCells_{k}"] = v
+                    
+                rows.append(row)
+                
+            return pd.DataFrame(rows)
+            
+        else:
+            return pd.DataFrame()
+  
+    _msg_dict = message_to_dict(msg_obj)
+    _msg_df =  dict_to_dataframe(_msg_dict)
+
+    if _msg_df.empty:
+        print('Error con mensaje:')
+        print(_msg_dict)
+        return pd.DataFrame()
+        
+    _msg_df.columns = _msg_df.columns.str.lower() 
+    _msg_df = _msg_df.round(0)           
+    _msg_df['date_starttime'] = pd.to_datetime(_msg_df['segmentstarttime'], unit='ms', utc=True)
+    _msg_df['id'] = _msg_df.apply(lambda x: str(x.nrcells_nrcelllabel) + '-' + str(x.imsi) + '-' + str(x.segmentstarttime), axis=1)
+    _msg_df = _msg_df[REQUIRED_COLS + ['id','date_starttime']]
+    _msg_df['broker_id'] = broker_id
+    
+    del _msg_df['segmentstarttime']
+    
+    return _msg_df
+
+
+
 class ConfluentKafkaSensor(BaseSensorOperator):
 
     @apply_defaults
-    def __init__(self, topic, kafka_config, max_messages=1, process_message_func=None, *args, **kwargs):
+    def __init__(self, topic, kafka_config, max_messages=1, process_message_func=None, broker_id, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
         self.topic = topic
@@ -81,6 +174,7 @@ class ConfluentKafkaSensor(BaseSensorOperator):
         self.max_messages = max_messages
         self.process_message_func = process_message_func
         self.message_count = 0  
+        self.broker_id = broker_id
 
     def poke(self, context):
         self.log.info("Polling Kafka topic: %s (processed %s/%s messages)", self.topic, self.message_count, self.max_messages)
@@ -88,6 +182,8 @@ class ConfluentKafkaSensor(BaseSensorOperator):
 
         consumer = Consumer(self.kafka_config)
         consumer.subscribe([self.topic])
+        
+        data_collected = []
         
         try:
 
@@ -102,29 +198,30 @@ class ConfluentKafkaSensor(BaseSensorOperator):
                     self.log.error("Consumer error: %s", msg.error())
                     break
                     
-
-                message = msg.value().decode("utf-8")
-                self.log.info("Received message: %s", message)
-                if self.process_message_func:
-                    self.process_message_func(message)
+                _msg_obj = DECO_NSA.FromString(msg.value())
+                _msg_df_append = self.process_message_func(_msg_obj,self.broker_id)
+                data_collected.append(_msg_df_append)
+                    
                 self.message_count += 1
 
             if self.message_count >= self.max_messages:
                 self.log.info("Processed maximum number of messages: %s", self.max_messages)
-                return True
-            return False  # Continue poking if we haven't reached the target count.
+            
+            DATA_COLLECTED_DF = pd.concat(data_collected)
+            
+            print('Uploading to Database: %s'%POSTGRES_IP)
+            try:
+                DATA_COLLECTED_DF.to_sql(POSTGRES_TABLE, POSTGRES_ENGINE, if_exists='append', index=False)     
+            except IntegrityError:
+                print('duplicated rows')
+            except ValueError as e:
+                print(e)
+                
         except KafkaException as e:
             self.log.error("Kafka exception occurred: %s", e)
             return False
         finally:
             consumer.close()
-
-
-
-
-
-
-
 
 @task(
     executor_config={'LocalExecutor': {}},
@@ -139,9 +236,6 @@ def initialization(ds=None, ti=None, **kwargs):
     return True
     
 
-
-
-
 with DAG(
     dag_id='stream_arieso',
     default_args={
@@ -153,6 +247,25 @@ with DAG(
     tags=['development', 'arieso', 'kafka'],
     catchup=False
     ) as dag:
+    
+        #with TaskGroup(group_id='dw_tasks') as dw_tasks:
+
+        #for i,server_conn in enumerate(PM_HUAWEI_SERVERS[:2]):
+    
+        #kafka_sensor_task = ConfluentKafkaSensor(
+        #    task_id="confluent_kafka_sensor",
+        #    topic="my_topic",  # Replace with your Kafka topic
+        #    kafka_config={
+        #        "bootstrap.servers": "localhost:9092",  # Your Kafka broker
+        #        "group.id": "airflow_sensor_group",
+        #        "auto.offset.reset": "earliest",
+        #    },
+        #    max_messages=5,  # Set the maximum number of messages to process
+        #    process_message_func=process_message,
+        #    mode="reschedule",  # Recommended to free up worker slots while waiting
+        #    poke_interval=10,   # Poll every 10 seconds
+        #    timeout=300         # Sensor will time out after 300 seconds if condition isn't met
+        #    )
     
         initialization() 
         
