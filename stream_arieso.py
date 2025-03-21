866b74bab4944d070ccb9fcc6a0dad1b01079659
Oci <valdo.masuero@gmail.com>
2025-03-21 10:51:12 -0300

    se amplia el tiemout

--- stream_arieso.py
+++ stream_arieso.py
@@ -259,7 +259,7 @@ with DAG(
     schedule_interval='*/15 * * * *',
     start_date=days_ago(1),
     max_active_runs= 1,
-    dagrun_timeout=timedelta(minutes=10),
+    dagrun_timeout=timedelta(minutes=30),
     tags=['development', 'arieso', 'kafka'],
     catchup=False
     ) as dag:
