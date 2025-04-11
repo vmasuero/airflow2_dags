45bde4d5c395d9600ac0f13012f8439561cf83a7
Oci <valdo.masuero@gmail.com>
2025-04-11 15:59:44 -0400

    se agrega ipload lickhpuse

--- dw_network_links_raw.py
+++ dw_network_links_raw.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import pendulum
 import os
 import re
+import boto3
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -22,6 +23,33 @@ BUCKET = 'bucket-scl-prod-monitoreosscc-datalake-001'
 ENDPOINT = "https://%s.compat.objectstorage.%s.oraclecloud.com"%(NAMESPACE,REGION)
 bucket_url = f'https://objectstorage.{REGION}.oraclecloud.com/n/{NAMESPACE}/b/{BUCKET}/o/arieso/tmp/file_arieso.csv'
 
+
+
+# CLICKHOUSE
+CLICKHOUSE_IP = 'clickhouse-counters.clickhouse.svc.cluster.local' 
+CLICKHOUSE_IP_SHARDS = ['chi-counters-counters-%s-0.clickhouse.svc.cluster.local'%x for x in [x for x in range(SHARDS)]]
+
+CLUSTER = 'counters'
+CLICKHOUSE_PORT = 8123      
+CLICKHOUSE_USERNAME = 'dev_user' 
+CLICKHOUSE_PASSWORD = 'vtrclaro1234'      
+DATABASE = 'OYM_COUNTERS' 
+TABLE = 'BW'
+TABLE_DIST = "%s_DIST"%TABLE
+CLUSTER = 'counters'
+
+
+
+def read_parquet_from_s3(path:str, s3_api):
+    obj_buffer = s3_api.Object(BUCKET, path)
+    
+    with BytesIO(obj_buffer.get()['Body'].read()) as buffer:
+        _df = pd.read_parquet(buffer)
+        
+    _df.reset_index(inplace=True)
+    
+    return _df
+    
 
 
 @task(
@@ -126,6 +154,85 @@ def download_files(ti=None,  **kwargs):
 
     return True
     
+
+@task(
+    executor_config={'LocalExecutor': {}},
+)
+def upload_clickhouse(ti=None,  **kwargs):
+
+    import boto3
+
+
+    def filter_links(desc:str):
+        _desc = desc.lower()
+
+        _regular_exps = [
+            r'link_[a-z]+_\d+.*',
+            r'bbolt_.*',
+            r'#ssip.*',
+            r'#isp.*',
+            r'#acceso.*',
+            r'#ntwk.*',
+            r'#mpls.*',
+            r'#tv.*',
+            r'#iptv.*',
+            r'cache.*'
+        ]
+        
+        for _reg in _regular_exps:          
+            if re.match(_reg,_desc):
+                return True   
+      
+        return False
+
+    def proc_traffic(data:pd.DataFrame):
+
+        MEGA = 1000000
+        
+        _traffic_df = data
+
+        _traffic_df = _traffic_df[_traffic_df.ifadmin == 1]
+        _traffic_df = _traffic_df[_traffic_df.ifoper == 1]
+        
+        _traffic_df['datetime'] = pd.to_datetime(_traffic_df['time'], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('America/Santiago')
+
+        _traffic_df['devif'] = pd.to_numeric(_traffic_df['devif'])
+        _traffic_df['input'] = (pd.to_numeric(_traffic_df['input']) )
+        _traffic_df['output'] = (pd.to_numeric(_traffic_df['output']) )
+        
+        _traffic_df.loc[_traffic_df['input'] < 0,'input'] = 0
+        _traffic_df.loc[_traffic_df['output'] < 0,'output'] = 0
+        
+        _traffic_df = _traffic_df.sort_values(by='datetime')
+        _traffic_df = _traffic_df[['devif','datetime','input','output']]
+
+        _traffic_df.devif = _traffic_df.devif.astype('int32')
+        _traffic_df.input = _traffic_df.input.astype('int64')
+        _traffic_df.output = _traffic_df.output.astype('int64')
+        _traffic_df['id'] = _traffic_df.devif.astype(str) + '-' + _traffic_df.datetime.dt.strftime('%Y%m%d%H%M%S')
+
+        return _traffic_df
+        
+    
+    _s3_api = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=REGION,
+        endpoint_url=ENDPOINT
+    )
+
+    _file_s3_traffic = ti.xcom_pull(task_ids='initialization', key='file_s3_traffic') 
+
+   _data_traffic = read_parquet_from_s3(_file_s3_traffic, _s3_api)
+   #_data_traffic = _data_traffic[ pd.notnull(_data_traffic.ifAlias)]
+   #_data_traffic = _data_traffic[ _data_traffic.ifAlias.apply(filter_links) ]
+   #_data_traffic = proc_traffic(_data_traffic)
+   
+   print(_data_traffic)
+   
+   return True
+        
 with DAG(
     dag_id='dw_network_links_raw',
     schedule_interval= "30 9 * * *",
@@ -142,4 +249,4 @@ with DAG(
 ) as dag:
 
    
-    initialization() >> check_files() >> download_files()
+    initialization() >> check_files() >> download_files() >> upload_clickhouse()
