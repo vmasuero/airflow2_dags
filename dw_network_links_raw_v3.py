a97caed9db3e0130c2db38fb4cfc3769bb2ae29c
Oci <valdo.masuero@gmail.com>
2025-09-23 12:28:20 -0300

    se trata de hcaer el upload

--- dw_network_links_raw_v3.py
+++ dw_network_links_raw_v3.py
@@ -36,6 +36,41 @@ S3_PATH = f"NETWORK_COUNTERS/OYM_v3"
 HEADERS_PATH = 'NETWORK_COUNTERS/HEADERS'
 DIARY_REPORT_DIR = f'NETWORK_COUNTERS/REPORT_DIARY_v3/{int(datetime.now().strftime('%Y'))}'
 
+
+SAMPLING = '5min'
+
+REMOTE_COLS = [
+    'devname',
+    'ifName',
+    'ifAlias',
+    'time',
+    'input',
+    'output',
+    'ifspeed',
+    'ifadmin',
+    'iftype',
+    'devif'
+]
+
+HEADERS_COLS = [
+        'Empresa', 
+        'Instancia 0', 
+        'Instancia 1', 
+        'Instancia 2',
+        'Localidad A', 
+        'Extremo A', 
+        'Pta A', 
+        'Descripcion',
+        'Localidad B',
+        'Extremo B', 
+        #'Pta B',
+        'Capacidad',
+        'Devif'
+]
+
+
+
+
 def read_parquet_s3( s3_api, path:str, bucket:str, cols=[]):
     obj_buffer = s3_api.Object(bucket, path)
 
@@ -48,6 +83,23 @@ def read_parquet_s3( s3_api, path:str, bucket:str, cols=[]):
 
     return _df 
     
+def read_csv_from_s3( s3_api, bucket, path:str):
+    obj_buffer = s3_api.Object(bucket, path)
+    
+    with BytesIO(obj_buffer.get()['Body'].read()) as buffer:
+        _df = pd.read_csv(buffer, sep=';')
+        
+    
+    return _df    
+
+
+def upload_parquet_s3(s3_api, bucket, data:pd.DataFrame, filename:str):
+
+    _parquet_buffer = BytesIO()
+    data.to_parquet(_parquet_buffer)
+    
+    _res = s3_api.Object(bucket, filename).put(Body=_parquet_buffer.getvalue())
+
     
 
 @task(
@@ -158,6 +210,138 @@ def dowload_upload_raw(yesterday_ds = None, ds=None, ti=None, ds_nodash=None,  *
     
     return True
     
+  
+@task(
+    executor_config={'LocalExecutor': {}},
+    pool='SERIAL'
+    
+)
+def create_daily_report(yesterday_ds = None, ds=None, ti=None, ds_nodash=None,  **kwargs):
+
+    def proc_header_file(data:pd.DataFrame):
+        _headers = data.copy()
+        _headers = _headers[pd.notnull(_headers['Extremo A'])]
+        
+        _headers = _headers[HEADERS_COLS]
+        _headers['Empresa'] = _headers['Empresa'].astype(str)
+        _headers['Instancia 0'] = _headers['Instancia 0'].astype(str)
+        _headers['Instancia 1'] = _headers['Instancia 1'].astype(str)
+        _headers['Instancia 2'] = _headers['Instancia 2'].astype(str) 
+        _headers['Localidad A'] = _headers['Localidad A'].astype(str) 
+        _headers['Extremo A'] = _headers['Extremo A'].str.lower().str.strip()
+        _headers['Extremo B'] = _headers['Extremo B'].str.lower().fillna('unknow').str.strip()
+        _headers['Pta A'] = _headers['Pta A'].str.lower().str.replace("'","").str.strip()
+        _headers['Localidad B'] = _headers['Localidad B'].astype(str).fillna('unknow').str.strip()
+        
+        _headers.loc[ _headers['Localidad A'] == 'nan', 'Localidad A'] = 'unknow'
+        _headers.loc[ _headers['Localidad B'] == 'nan', 'Localidad B'] = 'unknow'
+        
+        _headers['Descripcion'] = _headers['Descripcion'].astype(str)
+        _headers['Capacidad'] = _headers['Capacidad'].astype(float)
+        
+        _headers[_headers.select_dtypes('object').columns] = _headers[_headers.select_dtypes('object').columns].apply(lambda x: x.str.strip())
+
+
+        _headers = _headers[HEADERS_COLS]
+        _headers = _headers.set_index('Devif')
+
+
+        return _headers    
+
+    def proc_data_file(data:pd.DataFrame):
+        MEGA = 1000000
+
+        
+        def remove_outliners(g, cols=("input", "output"), factor=1.5):
+
+            for col in cols:
+                Q1 = g[col].quantile(0.25)
+                Q3 = g[col].quantile(0.75)
+                IQR = Q3 - Q1
+                #lower = Q1 - factor * IQR
+                upper = Q3 + factor * IQR
+        
+                mean_val = g[col].mean()
+        
+                #mask = (g[col] < lower) | (g[col] > upper)
+                mask = g[col] > upper
+
+                g.loc[mask, col] = int(mean_val)
+                
+            return g
+        
+        print('Filtering')
+        data =  data[data.devname != 'nan']
+        data =  data[data.ifAlias != 'nan']
+        data =  data[data.ifName != 'nan']
+        data = data[data.ifadmin == 1]
+        data = data.drop(['ifadmin'], axis=1)
+        data.devname = data.devname.apply(lambda x:  x.split('.')[0] )
+        data = data.set_index('devif')
+
+        print('Converting Dates')
+        data['time'] = pd.to_datetime(data['time'], format="%Y-%m-%d %H:%M:%S")
+        data['input'] = data['input'] / MEGA
+        data['output'] = data['input'] / MEGA
+        
+        print('Checking active links')
+        active_links = data[['input','output']].copy().max(axis=1).groupby(level=0).mean()
+        active_links = active_links[active_links > 5].index
+        data = data.loc[ data.index.isin(active_links) ]
+        
+        print('Removing outliners')
+        data = data.groupby(level=0, group_keys=False).apply(remove_outliners)
+
+        
+
+        return data
+
+
+    _remote_file = ti.xcom_pull(task_ids='initialization', key='remote_file') 
+    _remote_file_oci = ti.xcom_pull(task_ids='initialization', key='remote_file_oci') 
+    _header_file = ti.xcom_pull(task_ids='initialization', key='header_file')
+
+    _s3_api_oci = boto3.resource(
+        's3',
+        aws_access_key_id = OCI_ACCESS_KEY,
+        aws_secret_access_key = OCI_SECRET_KEY,
+        region_name = OCI_REGION, 
+        endpoint_url = OCI_ENDPOINT
+    )
+    
+    print(f"Reading file {_remote_file_oci}")
+    _traffic = read_parquet_from_s3(_s3_api_oci, OCI_BUCKET, _remote_file_oci, cols=REMOTE_COLS)
+    print(f"Reading file {_header_file}")
+    _header = read_csv_from_s3(_s3_api_oci, OCI_BUCKET, _header_file)
+
+    _header = proc_header_file(_header)
+    _traffic = _traffic[_traffic.devif.isin(_header.index)]
+    _traffic = proc_data_file(_traffic)
+
+    _traffic = pd.pivot_table(
+        _traffic, 
+        values=['input','output'], 
+        index=['devif'], 
+        columns=['time'], 
+        aggfunc="max"
+    ).stack(0, future_stack=True)
+
+    TIME_COLUMNS = _traffic.columns
+
+    _traffic = _traffic.reset_index()
+    _traffic = _traffic.rename(columns={'level_1':'Direccion'})
+    _traffic.Direccion = _traffic.Direccion.str.replace('input','IN')
+    _traffic.Direccion = _traffic.Direccion.str.replace('output','OUT')
+    _traffic = _traffic.set_index('devif')
+    _traffic = _header.join(_traffic)
+   
+    print(_traffic.sample(10))
+    return True
+    
+   
+
+
+
 with DAG(
     dag_id='dw_network_links_raw_v3',
     schedule_interval= "30 9 * * *",
